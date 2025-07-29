8b50db89bb725860f166d24aca418ba70b639e2b
Bhavya Rohilla <officialbhavyarohilla@gmail.com>
2025-07-29 22:30:15 +0530

    Structure-complete

---
+++ convex/schema.ts
@@ -0,0 +1,47 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+import { Content, Snippet } from "next/font/google";
+import { title } from "process";
+
+export default defineSchema({
+  users: defineTable({
+    userId: v.string(), // clerkId
+    email: v.string(),
+    name: v.string(),
+    isPro: v.boolean(),
+    proSince: v.optional(v.number()),
+    lemonSqueezyCustomerId: v.optional(v.string()),
+    lemonSqueezyOrderId: v.optional(v.string()),
+  }).index("by_user_id", ["userId"]),
+
+  codeExecutions: defineTable({
+    userId: v.string(),
+    langauge: v.string(),
+    code: v.string(),
+    output: v.optional(v.string()),
+    error: v.optional(v.string()),
+  }).index("by_user_id", ["userId"]),
+
+  snippets: defineTable({
+    userId: v.string(),
+    title: v.string(),
+    language: v.string(),
+    code: v.string(),
+    userName: v.string(),
+  }).index("by_user_id", ["userId"]),
+
+  SnippetComments: defineTable({
+    snippetId: v.id("snippets"),
+    userId: v.string(),
+    userName: v.string(),
+    content: v.string(),
+  }).index("by_snippet_id", ["snippetId"]),
+
+  stars: defineTable({
+    userId: v.id("users"),
+    snippetId: v.id("snippets"),
+  })
+    .index("by_user_id", ["userId"])
+    .index("by_snippet_id", ["snippetId"])
+    .index("by_user_id_and_snippet_id", ["userId", "snippetId"]),
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>home page</div>;
+  return (
+    <div>
+      <SignedOut>
+        <SignUpButton>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+
+      <UserButton />
+
+      <SignedIn>
+        <SignOutButton>
+          <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Sign Up
+          </button>
+        </SignOutButton>
+      </SignedIn>
+    </div>
+  );
 }
